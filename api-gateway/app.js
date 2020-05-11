145e474f373980d71651b0c2ef7886d2273512cc
therealjamesbrown <jamesbbb1@gmail.com>
2020-05-10 22:14:28 -0500

    adding 2.3

--- api-gateway/app.js
+++ api-gateway/app.js
@@ -16,6 +16,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
+var apiCatalog = require('./routes/api-catalog');
 mongoose.Promise = require('bluebird');
 
 var indexRouter = require('./routes/index');
@@ -40,6 +41,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/api', apiCatalog);
 
 app.use('/', indexRouter);
 
