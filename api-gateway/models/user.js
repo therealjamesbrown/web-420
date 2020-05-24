5c336ea33a03839f387817d011d3dfbdcc85d496
therealjamesbrown <jamesbbb1@gmail.com>
2020-05-24 17:23:56 -0500

    updating

--- api-gateway/models/user.js
+++ api-gateway/models/user.js
@@ -5,4 +5,13 @@ var userSchema = new mongoose.Schema({
     email: String
 });
 
+module.exports.add = (user, callback) => {
+    user.save(callback);
+}
+
+module.exports.getById = (id, callback) => {
+    var query = {_id: id};
+    User.findById(query, callback);
+};
+
 module.exports = mongoose.model('User', userSchema);
